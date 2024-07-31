e118ba54c0113daa6f6746fef897fec70e560ccc
Elzat Nurbek kyzy <elzatnurbekkyzy@Elzats-Air.hsd1.il.comcast.net>
2024-07-30 21:18:57 -0500

    updated

---
+++ src/test/java/utilities/Config.java
@@ -0,0 +1,37 @@
+package utilities;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Config {
+
+    private static Properties properties;
+
+
+    static {
+        String path = "src/test/resources/configurations.properties";
+
+        try{
+
+            FileInputStream file = new FileInputStream(path);
+            properties = new Properties();
+            properties.load(file);
+            file.close();
+        }catch ( IOException e){
+            System.out.println("We could not read that file" + path);
+            e.printStackTrace();//prints the error message
+        }
+
+
+    }
+
+    public static String getProperty (String key){
+       return  properties.getProperty(key);
+    }
+
+
+
+
+}
