9ab9d03efb6cfcdaef465fcfdcf1597125febf2b
xxlsbb <saitegin05@gmail.com>
2024-08-03 17:52:30 +0600

    login files

---
+++ src/test/java/utilities/CucumberRunner.java
@@ -0,0 +1,18 @@
+package utilities;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"html:target/cucumberReport.html","json:target/testRepot.json"},
+        features = "src/test/resources/features",
+        glue = "step_definitions",
+        tags = "@saucePositive",
+        dryRun = false
+)
+
+
+public class CucumberRunner {
+}
