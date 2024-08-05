6592e02fd8823d0f1f5c18023d28a63ba1c6af7d
KamilaB23 <evrfrfvf@mail.ru>
2024-08-05 19:31:16 -0400

    Update CucucmberRunner.java

--- src/test/java/CucucmberRunner.java
+++ src/test/java/CucucmberRunner.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {"html:target/cucumberReport.html", "json:target/testReport.json"},
-        features = "/Users/nurgazyamanbaev/Desktop/CashWiseCucumber/src/test/resources/features",
+        features = "src/test/resources/features",
         glue = "step_definitions",
         tags = "@AddingClient",
         dryRun = false
