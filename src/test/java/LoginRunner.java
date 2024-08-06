b2402a50325915a00af159181879e40a24b6480b
KamilaB23 <evrfrfvf@mail.ru>
2024-08-05 20:35:09 -0400

    Update LoginRunner.java

--- src/test/java/LoginRunner.java
+++ src/test/java/LoginRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 
         features = "src/test/resources/features",
         glue = "step_definitions",
-        tags = "@Archive",
+        tags = "@login",
         dryRun = false
 )
 public class LoginRunner {
