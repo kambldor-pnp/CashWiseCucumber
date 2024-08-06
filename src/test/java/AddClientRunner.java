7669d2a20d737d6c116caae086da74df8051927d
KamilaB23 <evrfrfvf@mail.ru>
2024-08-05 20:36:55 -0400

    Update AddClientRunner.java

--- src/test/java/AddClientRunner.java
+++ src/test/java/AddClientRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "step_definitions",
         tags = "@AddingClient",
-        dryRun = false
+        dryRun = true
 )
 
 
